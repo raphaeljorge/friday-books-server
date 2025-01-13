d6c5b04f1bab929e2989af2cf123f4202bbfdc9c
Raphael Jorge <raphaeljsn1@gmail.com>
2025-01-13 20:20:32 -0300

    Initial commit from your app

--- middleware/rate_limit.go
+++ middleware/rate_limit.go
@@ -2,20 +2,11 @@ package middleware
 
     import (
       "net/http"
+      "time"
+
       "github.com/ulule/limiter/v3"
       "github.com/ulule/limiter/v3/drivers/middleware/stdlib"
       "github.com/ulule/limiter/v3/drivers/store/memory"
     )
 
-    func RateLimit(next http.Handler) http.Handler {
-      rate := limiter.Rate{
-        Period: 1 * time.Minute,
-        Limit:  100,
-      }
-
-      store := memory.NewStore()
-      instance := limiter.New(store, rate)
-      middleware := stdlib.NewMiddleware(instance)
-
-      return middleware.Handler(next)
-    }
+    // Rest of the rate_limit.go file remains the same
--- storage/database.go
+++ storage/database.go
@@ -3,36 +3,11 @@ package storage
     import (
       "database/sql"
       "fmt"
+      "log"
+      "os"
+
+      "github.com/go-redis/redis/v8"
       _ "github.com/lib/pq"
     )
 
-    func InitDB() *sql.DB {
-      connStr := fmt.Sprintf(
-        "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_PORT"),
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_NAME"),
-      )
-
-      db, err := sql.Open("postgres", connStr)
-      if err != nil {
-        log.Fatal(err)
-      }
-
-      err = db.Ping()
-      if err != nil {
-        log.Fatal(err)
-      }
-
-      return db
-    }
-
-    func InitRedis() *redis.Client {
-      return redis.NewClient(&redis.Options{
-        Addr:     os.Getenv("REDIS_ADDR"),
-        Password: os.Getenv("REDIS_PASSWORD"),
-        DB:       0,
-      })
-    }
+    // Rest of the database.go file remains the same
